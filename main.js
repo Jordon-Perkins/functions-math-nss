b4eb74e9773143f1a51b96b921fcbd9d25949d34
Jordon Perkins <jordon.rubel@gmail.com>
2023-02-09 09:22:12 -0600

    only commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const add = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber;
+    return sum
+}
+
+let sum = add(4, 14)
+
+
+const multiply = (sum) => {
+    const sum2 = sum * 2
+    return sum2
+}
+
+let sum2 = multiply(sum, 2)
+
+// result = multiply(sum1)
+console.log(sum2)
